b2f4d8f9110282d87aa15bb0c2b3982b0eb08aa6
abdalfattah20 <abdalfattah.saleh@gmail.com>
2025-09-02 16:46:41 +0300

    Delete TEST.js

--- TEST.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.thumbnail').forEach(thumb => {
-                            thumb.addEventListener('click', () => {
-                                const mainImage = document.getElementById('mainImage');
-                                const tempSrc = mainImage.src;
-                                mainImage.src = thumb.src;
-                                thumb.src = tempSrc; // تبديل مع الرئيسية أو فقط نسخ (يمكن تعديل حسب الحاجة)
-                            });
-                        });
